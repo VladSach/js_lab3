88d53dcb9401b786f94b0ffd02c9f7fa1ed54c34
Vlad <vladsach@gmail.com>
2020-12-19 10:46:15 +0200

    hands-animation-complete

--- script.js
+++ script.js
@@ -12,6 +12,24 @@ function scrollFunction() {
         navbar.style.marginTop = "var(--header-hight)";
         navbar.style.height = "calc(100vh - var(--header-hight))";
     }
+
+    const box = document.getElementById('hand-box');
+    const left_hand =  document.getElementById('left-hand-id');
+    const right_hand =  document.getElementById('right-hand-id');
+    let animationShown = false;
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        if (!animationShown){
+            left_hand.style.left = '-19vw';
+            right_hand.style.right = '-19vw';
+            
+            box.style.animation = 'handBoxHid 6s';
+            left_hand.style.animation = 'leftHandOut 1s';
+            right_hand.style.animation = 'rightHandOut 1s';
+            animationShown = true;
+        }
+    }
+
+    box.style.opacity = '0';
 }
 
 const darkButton = document.getElementById('button-theme-dark');
